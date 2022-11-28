0bbf35fceb2581e42e9c2cdcd776633c7fd7953b
JonUCal <jonathan.rochette@ucalgary.ca>
2022-11-27 17:09:30 -0700

    created giftcard database

---
+++ com.diy.software/src/com/diy/software/fakedata/GiftcardDatabase.java
@@ -0,0 +1,15 @@
+package com.diy.software.fakedata;
+
+import java.util.HashMap;
+
+/*
+ * Used for storing data of gift cards and how much money is left in them
+ */
+public class GiftcardDatabase {
+	
+	/*
+	 * Hashmap holding all giftcard data
+	 */
+	public static final HashMap<String, Double> giftcardMap = new HashMap<String, Double>();
+	
+}
