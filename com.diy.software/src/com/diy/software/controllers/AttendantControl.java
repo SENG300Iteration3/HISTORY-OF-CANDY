244d8365c3aa8fcb5fcafd0bfd2f44e2d2762025
Di Nguyen <81270509+NguyenNhuDi@users.noreply.github.com>
2022-11-28 18:06:15 -0700

    added adjustCoinsForChange in attendant controller
    finished coinadjustment methods for attendant

--- com.diy.software/src/com/diy/software/controllers/AttendantControl.java
+++ com.diy.software/src/com/diy/software/controllers/AttendantControl.java
@@ -242,6 +242,9 @@ public class AttendantControl implements ActionListener, ReceiptPrinterListener 
 		//notify cash controller that the unit has been filled
 		sc.getCashControl().coinsLoaded(unit);
 		
+		//re enable system
+		sc.getCashControl().enablePayments();
+		
 		for (AttendantControlListener l : listeners)
 			l.initialState();
 	}
