527630513a58300b4cd8ac62469bca5cf2d050a8
elizaveta.khromova <elizaveta.khromova@cs.ucalgary.ca>
2022-11-30 16:07:55 -0700

    Created AddOwnBagsPromptScreen

--- com.diy.software/src/swing/screens/AddOwnBagsPromptScreen.java
+++ com.diy.software/src/swing/screens/AddOwnBagsPromptScreen.java
@@ -1,5 +1,6 @@
 package swing.screens;
 
+<<<<<<< Upstream, based on origin/main
 import com.diy.software.controllers.BagsControl;
 import com.diy.software.controllers.StationControl;
 import com.diy.software.listeners.BagsControlListener;
@@ -52,6 +53,35 @@ public class AddOwnBagsPromptScreen extends Screen implements BagsControlListene
 	public void readyToAcceptNewBagsInBaggingArea(BagsControl bc) {
 		//systemControl.goBackOnUI();
 		
+=======
+import com.diy.software.controllers.StationControl;
+
+import swing.styling.*;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class AddOwnBagsPromptScreen extends Screen {
+	private GUI_JLabel promptLabel;
+	private GUI_JButton doneAddingBags;
+
+	public AddOwnBagsPromptScreen(StationControl systemControl, String message) {
+		super(systemControl);
+		promptLabel = new GUI_JLabel(message);
+		promptLabel.setFont(GUI_Fonts.FRANKLIN_BOLD);
+
+		this.addLayer(promptLabel, 0);
+
+		doneAddingBags = new GUI_JButton();
+		doneAddingBags.setText("Done Adding Bags");
+		doneAddingBags.setBorder(BorderFactory.createMatteBorder(10, 20, 10, 20, GUI_Color_Palette.DARK_BLUE));
+		doneAddingBags.setPreferredSize(new Dimension(400, 100));
+
+		this.addLayer(doneAddingBags, 75);
+
+
+
+>>>>>>> 295e4e1 Created AddOwnBagsPromptScreen
 	}
 
 
