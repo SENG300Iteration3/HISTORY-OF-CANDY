a0d1f00c4559b53b28d9d7203a8e3abbde180869
Edvin <edvin.sinko@ucalgary.ca>
2022-12-01 14:42:18 -0700

    Finished CatalogPanel.java screen. It is now beautiful.

--- com.diy.software/src/swing/panels/CatalogPanel.java
+++ com.diy.software/src/swing/panels/CatalogPanel.java
@@ -1,10 +1,10 @@
 package swing.panels;
 
+import java.awt.*;
 import java.util.Map;
 import java.util.Map.Entry;
 
-import javax.swing.JButton;
-import javax.swing.JPanel;
+import javax.swing.*;
 
 import com.diy.hardware.BarcodedProduct;
 import com.diy.hardware.PLUCodedProduct;
@@ -17,10 +17,15 @@ import com.diy.software.listeners.ItemsControlListener;
 import com.jimmyselectronics.necchi.Barcode;
 import com.jimmyselectronics.necchi.BarcodedItem;
 import com.jimmyselectronics.necchi.Numeral;
+import swing.styling.GUI_Color_Palette;
+import swing.styling.GUI_Constants;
+import swing.styling.GUI_Fonts;
+import swing.styling.GUI_JButton;
 
 public class CatalogPanel extends JPanel implements ItemsControlListener{
 	private static final long serialVersionUID = 1L;
 	private ItemsControl ic;
+	private GUI_JButton cancelBtn;
 
 	public CatalogPanel(StationControl sc) {
 		super();
@@ -30,8 +35,35 @@ public class CatalogPanel extends JPanel implements ItemsControlListener{
 		Product product;
 		String strProductName;
 
-		JButton button;
-	
+		GUI_JButton button;
+
+
+		JLabel selectItemLabel = new JLabel("SELECT ITEM FROM CATALOGUE");
+		selectItemLabel.setFont(GUI_Fonts.TITLE);
+		selectItemLabel.setForeground(GUI_Color_Palette.WHITE);
+
+		JPanel catalogPanel = new JPanel(new BorderLayout());
+		catalogPanel.setBackground(Color.RED);
+		catalogPanel.setPreferredSize(new Dimension(GUI_Constants.SCREEN_WIDTH, GUI_Constants.SCREEN_HEIGHT-100));
+
+
+		JPanel selectItemTextPanel = new JPanel(new GridBagLayout());
+		selectItemTextPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		selectItemTextPanel.setPreferredSize(new Dimension(GUI_Constants.SCREEN_WIDTH, 70));
+		selectItemTextPanel.setBackground(GUI_Color_Palette.DARK_BLUE);
+		selectItemTextPanel.add(selectItemLabel);
+
+		catalogPanel.add(selectItemTextPanel, BorderLayout.PAGE_START);
+
+
+		this.add(catalogPanel);
+
+		JPanel buttonsPanel = new JPanel(new GridLayout(0, 2));
+		//buttonsPanel.setPreferredSize(new Dimension(GUI_Constants.SCREEN_WIDTH, 200));
+		//catalogPanel.add(buttonsPanel);
+
+		buttonsPanel.setBorder(BorderFactory.createEmptyBorder(10, 100, 50, 100));
+
 		// Iterate through Inventory to Create Buttons
 		for (Entry<Product, Integer> entry : ProductDatabases.INVENTORY.entrySet()) {
 			product = entry.getKey();
@@ -48,14 +80,35 @@ public class CatalogPanel extends JPanel implements ItemsControlListener{
 				System.out.println("PLUCodedProduct: " + strProductName);
 			}
 
-			button = new JButton(strProductName);
-			button.setActionCommand(strProductName);
-			button.addActionListener(ic);
-			this.add(button);
-			button.setEnabled(true);
+			JPanel panel = new JPanel(new GridLayout());
+			button = makeButton(strProductName, panel);
+			buttonsPanel.add(panel);
 
 		}
 
+		JScrollPane scrollPane = new JScrollPane(buttonsPanel);
+		catalogPanel.add(scrollPane, BorderLayout.CENTER);
+
+		JPanel cancelPanel = new JPanel(new GridLayout());
+		cancelPanel.setBorder(BorderFactory.createEmptyBorder(0, 100, 0, 100));
+		cancelBtn = makeButton("Go Back", cancelPanel);
+
+		catalogPanel.add(cancelPanel, BorderLayout.PAGE_END);
+
+
+	}
+
+	//This code is from AddItemsScreen.java
+	private GUI_JButton makeButton(String text, JPanel parent) {
+
+		GUI_JButton btn = new GUI_JButton(text.toUpperCase());
+		btn.setFont(GUI_Fonts.TITLE);
+		btn.setBorder(BorderFactory.createMatteBorder(10, 20, 10, 20, GUI_Color_Palette.DARK_BLUE));
+		btn.setBackground(GUI_Color_Palette.DARK_BROWN);
+		btn.setOpaque(true);
+		parent.add(btn);
+
+		return btn;
 	}
 
 	@Override
