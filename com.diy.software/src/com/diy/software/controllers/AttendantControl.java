acf9c55f6ed781b02354025e1b22afce037c4a0c
anthonyjdam <anthonydam1968@gmail.com>
2022-12-01 09:37:49 -0700

    Created a startUp method in AttendantControl

--- com.diy.software/src/com/diy/software/controllers/AttendantControl.java
+++ com.diy.software/src/com/diy/software/controllers/AttendantControl.java
@@ -41,6 +41,10 @@ public class AttendantControl implements ActionListener, ReceiptPrinterListener 
 		listeners.remove(l);
 	}
 
+	public void startUpStation() {
+		sc.startUp();
+	}
+	
 	public void approveBagsAdded() {
 		sc.unblockStation();
 		for (AttendantControlListener l : listeners) {
@@ -313,6 +317,10 @@ public class AttendantControl implements ActionListener, ReceiptPrinterListener 
 				case "prevent_use":
 					attendantNotifications = ("Preventing use on station for maintenance");
 					preventStationUse();
+				case "startUp":
+					System.out.println("Station has been started up");
+					startUpStation();
+					break;
 				default:
 					break;
 			}
