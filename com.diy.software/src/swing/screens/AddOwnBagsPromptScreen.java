092771c41a1c524bea1dc6432623546b9905924f
elizaveta.khromova <elizaveta.khromova@cs.ucalgary.ca>
2022-11-30 18:22:50 -0700

    Moved "use own bags" and "purchase bags" functionality to add item screen

--- com.diy.software/src/swing/screens/AddOwnBagsPromptScreen.java
+++ com.diy.software/src/swing/screens/AddOwnBagsPromptScreen.java
@@ -1,32 +1,57 @@
 package swing.screens;
 
+import com.diy.software.controllers.BagsControl;
 import com.diy.software.controllers.StationControl;
+import com.diy.software.listeners.BagsControlListener;
 
 import swing.styling.*;
 
 import javax.swing.*;
 import java.awt.*;
 
-public class AddOwnBagsPromptScreen extends Screen {
+public class AddOwnBagsPromptScreen extends Screen implements BagsControlListener{
 	private GUI_JLabel promptLabel;
 	private GUI_JButton doneAddingBags;
+	private BagsControl bc;
 
 	public AddOwnBagsPromptScreen(StationControl systemControl, String message) {
 		super(systemControl);
+		bc = systemControl.getBagsControl();
+		bc.addListener(this);
+		
 		promptLabel = new GUI_JLabel(message);
 		promptLabel.setFont(GUI_Fonts.FRANKLIN_BOLD);
 
 		this.addLayer(promptLabel, 0);
 
 		doneAddingBags = new GUI_JButton();
-		doneAddingBags.setText("Done Adding Bags");
+		doneAddingBags.setText("Done");
 		doneAddingBags.setBorder(BorderFactory.createMatteBorder(10, 20, 10, 20, GUI_Color_Palette.DARK_BLUE));
 		doneAddingBags.setPreferredSize(new Dimension(400, 100));
+		doneAddingBags.setActionCommand("done adding bags");
+		doneAddingBags.addActionListener(bc);
 
 		this.addLayer(doneAddingBags, 75);
 
+	}
 
+	@Override
+	public void awaitingCustomerToFinishPlacingBagsInBaggingArea(BagsControl bc) {
+		// TODO Auto-generated method stub
+		
+	}
 
+	@Override
+	public void awaitingAttendantToVerifyBagsPlacedInBaggingArea(BagsControl bc) {
+		doneAddingBags.setVisible(false);
+		promptLabel.setText("Please Wait For The Attendant's Approval");
+		
+	}
+
+	@Override
+	public void readyToAcceptNewBagsInBaggingArea(BagsControl bc) {
+		//systemControl.goBackOnUI();
+		
 	}
 
 
