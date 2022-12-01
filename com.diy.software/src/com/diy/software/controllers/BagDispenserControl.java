c1e771ce8baec403962558086aff3a88085ec61e
anhnguyen6535 <anhnguyen6535@gmail.com>
2022-12-01 15:47:41 -0700

    create action performed in BagDispenserControl

--- com.diy.software/src/com/diy/software/controllers/BagDispenserControl.java
+++ com.diy.software/src/com/diy/software/controllers/BagDispenserControl.java
@@ -4,16 +4,17 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
-import com.diy.software.listeners.MembershipControlListener;
+import com.diy.software.listeners.BagDispenserControlListener;
 import com.jimmyselectronics.AbstractDevice;
 import com.jimmyselectronics.AbstractDeviceListener;
 import com.jimmyselectronics.svenden.ReusableBagDispenser;
 import com.jimmyselectronics.svenden.ReusableBagDispenserListener;
 
-public class BagDispenserControl implements ActionListener, ReusableBagDispenserListener {
+public class BagDispenserControl implements ActionListener {
 	private StationControl sc;
-	private ArrayList<ReusableBagDispenserListener> listeners;
+	private ArrayList<BagDispenserControlListener> listeners;
 	private int numBag;
+	private String input = "";
 
 	public BagDispenserControl(StationControl sc) {
 		this.sc = sc;
@@ -23,56 +24,78 @@ public class BagDispenserControl implements ActionListener, ReusableBagDispenser
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		String c = e.getActionCommand();
-		if (c.startsWith("MEMBER_INPUT_BUTTON: ")) {
-			memberNumber += c.split(" ")[1];
-			for (MembershipControlListener l : listeners)
-				l.memberFieldHasBeenUpdated(this, memberNumber);
+		if (c.startsWith("NUMBER_BAGS: ")) {
+			input += c.split(" ")[1];
+			System.out.println(input);
+			for (BagDispenserControlListener l : listeners)
+				l.numberFieldHasBeenUpdated(this, input);
+		} else {
+			switch (c) {
+			case "cancel":
+				sc.goBackOnUI();
+				break;
+			case "correct":
+				if (input.length() > 0) {
+					input = input.substring(0, input.length() - 1);
+					for (BagDispenserControlListener l : listeners)
+						l.numberFieldHasBeenUpdated(this, input);
+				}
+				break;
+			case "submit":
+				numBag = Integer.parseInt(input); 
+				if(numBag > 0) {
+					//dispense
+				}
+				break;
+			default:
+				break;
+			}
 		}
 	}
 	
-	public void addListener(ReusableBagDispenserListener l) {
+	public void addListener(BagDispenserControlListener l) {
 		listeners.add(l);
 	}
 
-	@Override
-	public void enabled(AbstractDevice<? extends AbstractDeviceListener> device) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void disabled(AbstractDevice<? extends AbstractDeviceListener> device) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void turnedOn(AbstractDevice<? extends AbstractDeviceListener> device) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void turnedOff(AbstractDevice<? extends AbstractDeviceListener> device) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void bagDispensed(ReusableBagDispenser dispenser) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void outOfBags(ReusableBagDispenser dispenser) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void bagsLoaded(ReusableBagDispenser dispenser, int count) {
-		// TODO Auto-generated method stub
-		
-	}
+//	@Override
+//	public void enabled(AbstractDevice<? extends AbstractDeviceListener> device) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	@Override
+//	public void disabled(AbstractDevice<? extends AbstractDeviceListener> device) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	@Override
+//	public void turnedOn(AbstractDevice<? extends AbstractDeviceListener> device) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	@Override
+//	public void turnedOff(AbstractDevice<? extends AbstractDeviceListener> device) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	@Override
+//	public void bagDispensed(ReusableBagDispenser dispenser) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	@Override
+//	public void outOfBags(ReusableBagDispenser dispenser) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	@Override
+//	public void bagsLoaded(ReusableBagDispenser dispenser, int count) {
+//		// TODO Auto-generated method stub
+//		
+//	}
 }
