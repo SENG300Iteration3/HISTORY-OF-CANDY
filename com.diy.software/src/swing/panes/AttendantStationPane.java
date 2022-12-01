f4da2e8b0ab73a1e56bfdcedaa3fdd072d33b17e
anthonyjdam <anthonydam1968@gmail.com>
2022-12-01 10:31:44 -0700

    Updated the attendant screen

--- com.diy.software/src/swing/panes/AttendantStationPane.java
+++ com.diy.software/src/swing/panes/AttendantStationPane.java
@@ -3,9 +3,11 @@ package swing.panes;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.GridLayout;
 
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
+import javax.swing.JScrollPane;
 
 import com.diy.software.controllers.AttendantControl;
 import com.diy.software.controllers.BagsControl;
@@ -17,6 +19,7 @@ import swing.styling.GUI_Color_Palette;
 import swing.styling.GUI_Fonts;
 import swing.styling.GUI_JButton;
 import swing.styling.GUI_JLabel;
+import swing.styling.GUI_JPanel;
 import swing.styling.Screen;
 
 public class AttendantStationPane extends Screen implements AttendantControlListener, BagsControlListener {
@@ -29,7 +32,8 @@ public class AttendantStationPane extends Screen implements AttendantControlList
 	GUI_JButton addInkToPrinterButton;
 	GUI_JButton addPaperToPrinterButton;
 	GUI_JButton approveNoBagging;
-	GUI_JLabel weightDisplayLabel, weightDescrepancyMssg;
+	GUI_JButton startUpButton;
+	GUI_JLabel weightDisplayLabel, weightDescrepancyMssg, lowInkLabel, lowPaperLabel, adjustCoinLabel, adjustBanknoteLabel;
 
 	private static String HeaderText = "Attendant Screen";
 
@@ -42,53 +46,96 @@ public class AttendantStationPane extends Screen implements AttendantControlList
 		ac = sc.getAttendantControl();
 		ac.addListener(this);
 
-		int width = 450;
+		int width = 400;
 		int height = 50;
 
-		approveAddedBagsButton = createPinPadButton("approvedAddedBags()");
+		approveAddedBagsButton = makeButton("approvedAddedBags()");
 		approveAddedBagsButton.setActionCommand("approve added bags");
 		approveAddedBagsButton.addActionListener(ac);
 		approveAddedBagsButton.setPreferredSize(new Dimension(width, height));
 
-		addInkToPrinterButton = createPinPadButton("Add ink");
+		addInkToPrinterButton = makeButton("Add ink");
 		addInkToPrinterButton.setActionCommand("addInk");
 		addInkToPrinterButton.addActionListener(ac);
 		addInkToPrinterButton.setPreferredSize(new Dimension(width, height));
 		
-		addPaperToPrinterButton = createPinPadButton("Add paper");
+		addPaperToPrinterButton = makeButton("Add paper");
 		addPaperToPrinterButton.setActionCommand("addPaper");
 		addPaperToPrinterButton.addActionListener(ac);
 		addPaperToPrinterButton.setPreferredSize(new Dimension(width, height));
 		
-		approveNoBagging = createPinPadButton("Approve no bagging");
+		approveNoBagging = makeButton("Approve no bagging");
 		approveNoBagging.setActionCommand("no_bagging");
 		approveNoBagging.addActionListener(ac);
 		approveNoBagging.setPreferredSize(new Dimension(width, height));
-
-		weightDescrepancyMssg = initalizeLabel();
-		weightDisplayLabel = initalizeLabel();
 		
-		addLayer(approveAddedBagsButton, 0);
-		addLayer(addInkToPrinterButton,0);
-		addLayer(addPaperToPrinterButton,0);
-		addLayer(approveNoBagging,0);
-		addLayer(weightDescrepancyMssg,0);
+		startUpButton = makeButton("Start up station");
+		startUpButton.setActionCommand("startUp");
+		startUpButton.addActionListener(ac);
+		startUpButton.setPreferredSize(new Dimension(width, height));
+		
+		weightDescrepancyMssg = initalizeLabel("weightDiscrepancyMsg");
+		weightDisplayLabel = initalizeLabel("weightDisplayLabel");
+		lowInkLabel = initalizeLabel("Low ink");
+		lowPaperLabel = initalizeLabel("Low paper");
+		adjustCoinLabel = initalizeLabel("Adjust coin");
+		adjustBanknoteLabel = initalizeLabel("Adjust Banknote");
+		
+		
+		GUI_JPanel notificationPanel = new GUI_JPanel();
+		notificationPanel.setLayout(new GridLayout(2, 3));
+		
+		notificationPanel.add(weightDescrepancyMssg);
+		notificationPanel.add(weightDisplayLabel);
+		notificationPanel.add(lowInkLabel);
+		notificationPanel.add(lowPaperLabel);
+		notificationPanel.add(adjustCoinLabel);
+		notificationPanel.add(adjustBanknoteLabel);
+		
+		this.addLayer(notificationPanel, 0);
+		
+		
+		
+		JScrollPane buttonScrollPane = new JScrollPane(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+				JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		buttonScrollPane.getVerticalScrollBar().setPreferredSize(new Dimension(24, 0));
+		buttonScrollPane.setBackground(GUI_Color_Palette.DARK_BROWN);
+		buttonScrollPane.setPreferredSize(new Dimension(450, 200));
+		buttonScrollPane.setBorder(BorderFactory.createMatteBorder(20, 20, 20, 20, GUI_Color_Palette.DARK_BLUE));
+		
+		
+		GUI_JPanel buttonsPanel	= new GUI_JPanel();
+		buttonsPanel.setLayout(new GridLayout(5, 1));
+		buttonScrollPane.getViewport().add(buttonsPanel);
+		
+		buttonsPanel.add(approveAddedBagsButton);
+		buttonsPanel.add(addInkToPrinterButton);
+		buttonsPanel.add(addPaperToPrinterButton);
+		buttonsPanel.add(approveNoBagging);
+		buttonsPanel.add(startUpButton);
+		
+		
+		this.addLayer(buttonScrollPane, 50);
+
 		
 		addInkToPrinterButton.setEnabled(false);
 		addPaperToPrinterButton.setEnabled(false);
 		approveNoBagging.setEnabled(false);
 		approveAddedBagsButton.setEnabled(cusAddedBags);
+		startUpButton.setEnabled(true);
 	}
-
-	private GUI_JLabel initalizeLabel() {
+	
+	
+	private GUI_JLabel initalizeLabel(String labelText) {
 
 		GUI_JLabel label = new GUI_JLabel();
+		label.setText(labelText);
 		label.setForeground(GUI_Color_Palette.WHITE);
-		label.setPreferredSize(new Dimension(this.width - 200, 100));
+		label.setBackground(GUI_Color_Palette.LIGHT_BROWN);
+		label.setPreferredSize(new Dimension(300, 100));
 		label.setFont(GUI_Fonts.FRANKLIN_BOLD);
 		label.setHorizontalAlignment(JLabel.CENTER);
-
-		addLayer(label, 0);
+		label.setBorder(BorderFactory.createMatteBorder(20, 20, 20, 20, GUI_Color_Palette.DARK_BLUE));
 
 		return label;
 	}
@@ -117,22 +164,22 @@ public class AttendantStationPane extends Screen implements AttendantControlList
 
 	}
 
-	private GUI_JButton createPinPadButton(String text) {
+	private GUI_JButton makeButton(String text) {
 		int overallMargin = 10;
 
 		/* Setup of the title's panel */
-		GUI_JButton pinPadButton = new GUI_JButton();
-		pinPadButton.setText(text);
-		pinPadButton.setBackground(GUI_Color_Palette.DARK_BROWN);
-		pinPadButton.setForeground(GUI_Color_Palette.WHITE);
+		GUI_JButton button = new GUI_JButton();
+		button.setText(text);
+		button.setBackground(GUI_Color_Palette.DARK_BROWN);
+		button.setForeground(GUI_Color_Palette.WHITE);
 
-		pinPadButton.setFont(new Font("Franklin Gothic", Font.BOLD, 22));
+		button.setFont(new Font("Franklin Gothic", Font.BOLD, 22));
 
-		pinPadButton.setBorder(BorderFactory.createLineBorder(GUI_Color_Palette.DARK_BLUE, overallMargin));
-		pinPadButton.setLayout(new BorderLayout());
+		button.setBorder(BorderFactory.createLineBorder(GUI_Color_Palette.DARK_BLUE, overallMargin));
+		button.setLayout(new BorderLayout());
 
 		/* Adding the panel to the window */
-		return pinPadButton;
+		return button;
 	}
 
 	@Override
@@ -177,5 +224,11 @@ public class AttendantStationPane extends Screen implements AttendantControlList
 		addPaperToPrinterButton.setEnabled(false);	
 		approveNoBagging.setEnabled(false);
 		weightDescrepancyMssg.setText("");
+	}
+	
+	public static void main(String args[]) {
+		StationControl sc = new StationControl();
+		AttendantStationPane ap = new AttendantStationPane(sc);
+		ap.openInNewJFrame();
 	}
 }
