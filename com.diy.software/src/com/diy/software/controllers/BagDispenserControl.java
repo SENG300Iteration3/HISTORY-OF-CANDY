d2d5b9521077243afb3f8e55f98e3179d9d1661a
anhnguyen6535 <anhnguyen6535@gmail.com>
2022-12-01 18:54:31 -0700

    Update BagDispenserControl

--- com.diy.software/src/com/diy/software/controllers/BagDispenserControl.java
+++ com.diy.software/src/com/diy/software/controllers/BagDispenserControl.java
@@ -7,18 +7,53 @@ import java.util.ArrayList;
 import com.diy.software.listeners.BagDispenserControlListener;
 import com.jimmyselectronics.AbstractDevice;
 import com.jimmyselectronics.AbstractDeviceListener;
+import com.jimmyselectronics.EmptyException;
+import com.jimmyselectronics.svenden.ReusableBag;
 import com.jimmyselectronics.svenden.ReusableBagDispenser;
 import com.jimmyselectronics.svenden.ReusableBagDispenserListener;
 
 public class BagDispenserControl implements ActionListener {
 	private StationControl sc;
+	private ItemsControl ic;
 	private ArrayList<BagDispenserControlListener> listeners;
 	private int numBag;
 	private String input = "";
+	private ReusableBag lastDispensedReusableBag;
 
 	public BagDispenserControl(StationControl sc) {
 		this.sc = sc;
+		this.ic = sc.getItemsControl();
 		this.listeners = new ArrayList<>();
+	}
+	
+	private void dispenseBag() {
+		resetInput();
+		
+		while(numBag > 0) {
+			try {
+				System.out.println("purchase: " + numBag);
+				// add the dispensed bag to checkout item list
+				lastDispensedReusableBag = sc.station.reusableBagDispenser.dispense();
+				
+				// update station expected weight and add bag on the bagging area
+				sc.addReusableBag(lastDispensedReusableBag);
+				sc.station.baggingArea.add(lastDispensedReusableBag);
+				
+				// notify item control of new bag dispensed
+				ic.addReusableBags(lastDispensedReusableBag);
+				numBag--;
+				
+			} catch (EmptyException e) {
+//				System.out.println("Not enough bag. Only " + (numBag - i) + " bag available.");
+			}
+		}
+		sc.goBackOnUI();
+	}
+	
+	private void resetInput() {
+		for (BagDispenserControlListener l : listeners)
+			l.numberFieldHasBeenUpdated(this, "");
+		input = "";
 	}
 
 	@Override
@@ -26,11 +61,13 @@ public class BagDispenserControl implements ActionListener {
 		String c = e.getActionCommand();
 		if (c.startsWith("NUMBER_BAGS: ")) {
 			input += c.split(" ")[1];
-			System.out.println(input);
 			for (BagDispenserControlListener l : listeners)
 				l.numberFieldHasBeenUpdated(this, input);
 		} else {
 			switch (c) {
+			case "purchase bags":
+				sc.startPurchaseBagsWorkflow();
+				break;
 			case "cancel":
 				sc.goBackOnUI();
 				break;
@@ -44,7 +81,7 @@ public class BagDispenserControl implements ActionListener {
 			case "submit":
 				numBag = Integer.parseInt(input); 
 				if(numBag > 0) {
-					//dispense
+					dispenseBag();
 				}
 				break;
 			default:
@@ -56,46 +93,4 @@ public class BagDispenserControl implements ActionListener {
 	public void addListener(BagDispenserControlListener l) {
 		listeners.add(l);
 	}
-
-//	@Override
-//	public void enabled(AbstractDevice<? extends AbstractDeviceListener> device) {
-//		// TODO Auto-generated method stub
-//		
-//	}
-//
-//	@Override
-//	public void disabled(AbstractDevice<? extends AbstractDeviceListener> device) {
-//		// TODO Auto-generated method stub
-//		
-//	}
-//
-//	@Override
-//	public void turnedOn(AbstractDevice<? extends AbstractDeviceListener> device) {
-//		// TODO Auto-generated method stub
-//		
-//	}
-//
-//	@Override
-//	public void turnedOff(AbstractDevice<? extends AbstractDeviceListener> device) {
-//		// TODO Auto-generated method stub
-//		
-//	}
-//
-//	@Override
-//	public void bagDispensed(ReusableBagDispenser dispenser) {
-//		// TODO Auto-generated method stub
-//		
-//	}
-//
-//	@Override
-//	public void outOfBags(ReusableBagDispenser dispenser) {
-//		// TODO Auto-generated method stub
-//		
-//	}
-//
-//	@Override
-//	public void bagsLoaded(ReusableBagDispenser dispenser, int count) {
-//		// TODO Auto-generated method stub
-//		
-//	}
 }
