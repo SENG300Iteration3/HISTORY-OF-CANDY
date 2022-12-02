082007cbe4d70405cfbc979b4cbaeec27ecb579f
anthonyjdam <anthonydam1968@gmail.com>
2022-12-01 19:41:05 -0700

    Update AttendantControl.java

--- com.diy.software/src/com/diy/software/controllers/AttendantControl.java
+++ com.diy.software/src/com/diy/software/controllers/AttendantControl.java
@@ -334,10 +334,6 @@ public class AttendantControl implements ActionListener, ReceiptPrinterListener 
 					System.out.println("Station has been started up");
 					startUpStation();
 					break;
-				case "startUp":
-					System.out.println("Station has been started up");
-					startUpStation();
-					break;
 				default:
 					break;
 			}
