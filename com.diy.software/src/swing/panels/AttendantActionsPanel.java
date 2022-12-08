46e27f464c1199875e49da750899cb3df47ee00b
anhnguyen6535 <anhnguyen6535@gmail.com>
2022-12-07 18:37:56 -0700

    ad unimplemented methods

--- com.diy.software/src/swing/panels/AttendantActionsPanel.java
+++ com.diy.software/src/swing/panels/AttendantActionsPanel.java
@@ -318,5 +318,11 @@ public class AttendantActionsPanel extends JPanel
 		// TODO Auto-generated method stub
 		
 	}
+
+	@Override
+	public void banknotesInStorageLowState() {
+		// TODO Auto-generated method stub
+		
+	}
 }
 
