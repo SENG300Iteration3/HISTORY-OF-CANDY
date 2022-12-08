856d902e8b57b9c6dcbbb6a278d84b72bb449ec3
anhnguyen6535 <anhnguyen6535@gmail.com>
2022-12-07 19:10:59 -0700

    Rehandle noBagRequest Exception

--- com.diy.software/src/swing/panels/AttendantPanel.java
+++ com.diy.software/src/swing/panels/AttendantPanel.java
@@ -208,8 +208,7 @@ public class AttendantPanel extends JPanel implements AttendantControlListener, 
 
 	@Override
 	public void itemBagged() {
-		// TODO Auto-generated method stub
-		
+		approveNoBagButton.setEnabled(false);
 	}
 
 	@Override
